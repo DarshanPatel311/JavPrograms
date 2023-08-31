11871e17d742944b26bb079ca248e71ff23381a1
Darshan Patel <dkpatel3116@gmail.com>
2023-08-31 09:30:27 +0530

    31-08-2023

--- src/Pattern2.java
+++ src/Pattern2.java
@@ -1,2 +1,11 @@
 public class Pattern2 {
+    public static void main(String[] args)
+    {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+    }
 }

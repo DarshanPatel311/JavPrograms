269e6d6767ec975fed0a8554924f086bd0fc0634
Darshan Patel <dkpatel3116@gmail.com>
2023-08-31 09:27:30 +0530

    31-08-2023

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -1,6 +1,13 @@
 public class Pattern1 {
     public static void main(String[] args)
     {
-        System.out.println("Pattern 2");
+        System.out.println("- - -");
+        System.out.println("|    |");
+        System.out.println("N    |");
+        System.out.println("A    |");
+        System.out.println("M    |");
+        System.out.println("E    |");
+        System.out.println("|    |");
+        System.out.println("- - -");
     }
 }
